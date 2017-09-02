121f318993e87305905d6cba3420fb2ef204b146
Bonitamoma <mrswife.lady@gmail.com>
2017-09-02 10:44:20 -0500

    fixing

---
+++ wp-content/themes/accelerate-theme-child/single-projects.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content">
+		<div class="main-content" role="main">
+		
+			<?php while ( have_posts() ) : the_post(); ?>
+			$project_name = get_field('project_name');
+			$client = get_field('client');
+			$link = get_field('site_link');
+			$image_1 = get_field('image_1');
+			$image_2 = get_field('image_2');
+			$image_3 = get_field('image_3');
+			<article class="Projects">
+				<aside class="Projects">
+					<h2><?php the_title(); ?></h2>
+					<h5><?php echo $Project_name; ?></5>
+					<h6>Client: <?php echo $client; ?></h6>
+				<?php the_content(); ?>
+				<p><a href="<?php echo $link; ?>">Site Link</a></p>
+				</aside>
+				<div class="Project-images">
+					<img src="<?php echo $image_1; ?>" />
+					<img src="<?php echo $image_2; ?>" />
+					<img src="<?php echo $image_3; ?>" />
+				</div>
+			</article>
+			<?php endwhile; // end of the loop. ?>
+		</div><!-- .main-content -->
+
+		
+
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
