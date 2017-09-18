d3a152533d60cfe227880a64e39c5ad261794729
Bonitamoma <mrswife.lady@gmail.com>
2017-09-18 13:43:49 -0500

    blogger added and custom archive page done

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -1,57 +1,56 @@
 <?php
 /**
- * The template for displaying Case Studies Pages
+ * The template for displaying a single case study.
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
  *
  * @package WordPress
  * @subpackage Accelerate Marketing
  * @since Accelerate Marketing 2.0
  */
-
 get_header(); ?>
 
 <div id="primary" class="site-content">
 	<div class="main-content" role="main">
-			
-<?php while ( have_posts() ) : the_post(); 
-	$services = get_field('services');
-	$client = get_field('client');
-	$link = get_field('site_link');
-	$image1 = get_field ('image1');
-	$image2 = get_field ('image2');
-	$image3 = get_field ('image3'); ?>
-			
-	<article class="case-study">
+
+	<?php while ( have_posts() ) : the_post();
+		$services = get_field('services');
+		$client = get_field('client');
+		$link = get_field('site_link');
+		$image1 = get_field('image1');
+		$image2 = get_field('image2');
+		$image3 = get_field('image3');
+		$size = "full";
+		?>
+
+	<article class="case-study clearfix">
 		<aside class="case-study-sidebar">
 			<h2><?php the_title(); ?></h2>
-				<h4><?php echo $services; ?></h4>
-					<h4><span>Client: <?php echo $client; ?></span></h4>
-						
-				<?php the_content(); ?>
-			
-			<p><strong><a href="<?php echo $link; ?>">Site Link</a></strong></p>
+			<h4><span><?php echo $services; ?></span></h4>
+			<h4>Client: <?php echo $client; ?></h4>
+
+			<?php the_content(); ?>
+
+			<p class="read-more-link"><a href="<?php echo $link; ?>">Visit Live Site &rsaquo;</a></p>
 		</aside>
-			<div class="case-study-images">
-			<?php if(image1) { ?> 
-			<img src="<?php echo $image1; ?>" />
-			<?php } ?>
-			<?php if(image2) { ?> 
-			<img src="<?php echo $image2; ?>" />
-			<?php } ?>
-			<?php if(image3) { ?> 
-			<img src="<?php echo $image3; ?>" />
-			<?php } ?>
-			</div>
-			
-			</article>
-			<?php endwhile; // end of the loop. ?>
-		</div><!-- .main-content -->
 
+		<div class="case-study-images">
+			<?php if($image1) {
+				echo wp_get_attachment_image( $image1, $size );
+			} ?>
+			<?php if($image2) {
+				echo wp_get_attachment_image( $image2, $size );
+			} ?>
+			<?php if($image3) {
+				echo wp_get_attachment_image( $image3, $size );
+			} ?>
+		</div>
+	</article>
+		<?php endwhile; // end of the loop. ?>
+	</div><!-- .main-content -->
+</div><!-- #primary -->
 
-	</div><!-- #primary -->
+<div id="navigation" class="container">
+	<div class="left"><a href="<?php echo site_url('/case-studies/') ?>">&larr; <span>Back to Work</span></a></div>
+</div>
 
 <?php get_footer(); ?>
